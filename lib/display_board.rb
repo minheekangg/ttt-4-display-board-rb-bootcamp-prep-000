93fb53487593ad84d6e6d64e7cddc39201d5c8ac
Min Hee  Park <minheekangg@gmail.com>
2018-08-23 01:16:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,8 +4,8 @@
   board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  "-----------"
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  "-----------"
-  " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
